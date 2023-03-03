8b683ad09964dc2ba5ee721f092a1c5ab201df3a
Charleswcl <wang90cl@hotmail.com>
2023-03-03 09:28:24 +0800

    Initial commit

---
+++ src/main/java/batch02_ssf_assessment/ssf/assessment/SsfAssessmentApplication.java
@@ -0,0 +1,13 @@
+package batch02_ssf_assessment.ssf.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsfAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/batch02_ssf_assessment/ssf/assessment/SsfAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package batch02_ssf_assessment.ssf.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
